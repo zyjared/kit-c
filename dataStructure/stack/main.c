4c5f4ba56e2dca27019f6ef080bdd3da5bdbfd8e
zyjared <zyjared@outlook.com>
2024-07-22 19:11:13 +0800

    add: dataStructure > stack

--- dataStructure/stack/main.c
+++ dataStructure/stack/main.c
@@ -1,45 +1,114 @@
 /*
- * initStack(&S);
+ * 栈
  *
- * destroyStack(&S);
- *
- * clearStack(&S);
- *
- * stackEmpty(S);
- *
- * stackLength(S);
- *
- * getTop(S, &e);
- *
- * push(&S, e);
- *
- * pop(&S, &e);
- *
- * stackTraverse(S);
+ * Stack
  */
 
 #include <stdio.h>
-#include "utils.h"
+#include <stdlib.h>
 
-SElemType* createEmptySElem() {
-    SElemType* e = (SElemType*)malloc(sizeof(SElemType));
+#define INIT_SIZE 20
+#define INCREMENT_SIZE 10
+
+typedef enum {
+    ERROR,
+    OK
+} Status;
+
+typedef int ElemType;
+
+typedef struct {
+    ElemType* base;
+    ElemType* top;
+    int stackSize;
+} SqStack;
+
+// ------------------------------------------
+// utils
+// ------------------------------------------
+
+ElemType* createEmptySElem() {
+    ElemType* e = (ElemType*)malloc(sizeof(ElemType));
     if (e == NULL) exit(1);
 
     return e;
 }
 
+void printStack(SqStack S) {
+    for (ElemType* p = S.base; p < S.top; p++) {
+        printf("%d ", *p);
+    }
+    printf("\n");
+}
+
+// ------------------------------------------
+// ------------------------------------------
+
 Status initStack(SqStack* S) {
-    S->base = (SElemType*)malloc(INIT_SIZE * sizeof(SElemType));
+    S->base = (ElemType*)malloc(INIT_SIZE * sizeof(ElemType));
+    if (S->base == NULL) exit(1);
+
     S->top = S->base;
     S->stackSize = INIT_SIZE;
 
     return OK;
 }
 
-Status getTop(SqStack S, SElemType* e) {
-    if (S.top == S.base) return ERROR;
+Status stackLength(SqStack S) {
+    return S.top - S.base;
+}
+
+Status getTop(SqStack S, ElemType* e) {
+    if (S.top == S.base) return ERROR;          // 栈为空
+    *e = *(S.top - 1);                          // 返回栈顶元素
+    return OK;
+}
+
+Status push(SqStack* S, ElemType e) {
+    if (S->top - S->base >= S->stackSize) {
+        S->base = (ElemType*)realloc(S->base, (S->stackSize + INCREMENT_SIZE) * sizeof(ElemType));
+        if (!S->base) exit(1);
+
+        S->top = S->base + S->stackSize;
+        S->stackSize += INCREMENT_SIZE;
+    }
+    *S->top++ = e;
+    return OK;
+}
+
+Status pop(SqStack* S, ElemType* e) {
+    if (S->top == S->base) return ERROR;
+    *e = *--S->top;
+    return OK;
 }
 
 int main() {
-    printf("Hello, World!\n");
+
+    SqStack S;
+
+    initStack(&S);
+
+    // ------------- push ---------------
+
+    push(&S, 1);
+    push(&S, 2);
+    push(&S, 3);
+
+    printf("\npush:\n");
+    printf("  length               : %d\n", stackLength(S));
+    printf("  stack                : ");
+    printStack(S);
+
+    // ------------- pop ---------------
+
+    ElemType e;
+    pop(&S, &e);
+
+    printf("\npop:\n");
+    printf("  elem                 : %d\n", e);
+    printf("  length               : %d\n", stackLength(S));
+    printf("  stack                : ");
+    printStack(S);
+
+    return 0;
 }
